3533d500cbefe8bf5edc8a24b3842ee5e5ca77bd
Nigel Greenway <nigel_greenway@icloud.com>
2014-04-06 22:05:22 +0100

    Remove the cowardliness...
    I was being a coward by using `sudo`.

--- functions/install--tools__dev.sh
+++ functions/install--tools__dev.sh
@@ -2,44 +2,44 @@
 # Development tools
 
 # prereq's
-sudo brew install gnutls
-sudo brew install cmake
+brew install gnutls
+brew install cmake
 
 # git++-
-sudo brew install git
+brew install git
 
 # Terminal based apps
-sudo brew cask install iterm2
-sudo brew cask install Macvim
-sudo brew install trash
-sudo brew install terminal-notifier # https://github.com/alloy/terminal-notifier/blob/master/README.markdown
-sudo brew install zsh
-sudo brew install coreutils
-sudo brew install irssi
-sudo brew install task
-sudo brew cask install vagrant
-sudo brew cask install virtualbox
+brew cask install iterm2
+brew cask install Macvim
+brew install trash
+brew install terminal-notifier # https://github.com/alloy/terminal-notifier/blob/master/README.markdown
+brew install zsh
+brew install coreutils
+brew install irssi
+brew install task
+brew cask install vagrant
+brew cask install virtualbox
 
 # Reference tools
-sudo brew cask install Colours
+brew cask install Colours
 
 # Ruby tools
-sudo brew cask install Jewelrybox
+sudobrew cask install Jewelrybox
 curl -L https://get.rvm.io | bash -s stable --ruby --rails
 source ~/.rvm/scripts/rvm
 rvm --default use system
 
 # Node tools
-sudo brew install node
+brew install node
 
 # MySql
-sudo brew cask install SequelPro
+brew cask install SequelPro
 
 # Install editors
-sudo brew cask install sublime-text-3
+brew cask install sublime-text-3
 ln -s /Users/whoami/Applications/Sublime\ Text.app/Contents/SharedSupport/bin/subl /usr/local/bin/subl # Link `subl` binary
 echo "Setup Sublime text"
 echo "Add license key"
 read
 
-sudo brew cask install Textmate # open license key file
+brew cask install Textmate # open license key file
