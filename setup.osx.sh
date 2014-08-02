ceb1b94649743d3f0530259d1d4bd2c5618d5230
Nigel Greenway <nigel_greenway@me.com>
2014-08-02 23:29:27 +0100

    New funky version of my install script

---
+++ setup.osx.sh
@@ -0,0 +1,46 @@
+# Disable the “Are you sure you want to open this application?” dialog
+defaults write com.apple.LaunchServices LSQuarantine -bool false
+spctl --master-disable
+
+# Enable full keyboard access for all controls
+# (e.g. enable Tab in modal dialogs)
+defaults write NSGlobalDomain AppleKeyboardUIMode -int 3
+
+# Don’t show Dashboard as a Space
+defaults write com.apple.dashboard mcx-disabled -boolean true
+
+# Trackpad: enable tap to click for this user and for the login screen
+defaults write com.apple.driver.AppleBluetoothMultitouch.trackpad Clicking -bool true
+defaults -currentHost write NSGlobalDomain com.apple.mouse.tapBehavior -int 1
+defaults write NSGlobalDomain com.apple.mouse.tapBehavior -int 1
+# Allow Three Finger Drag on the trackpad
+defaults write com.apple.driver.AppleBluetoothMultitouch.trackpad TrackpadThreeFingerDrag -bool true
+defaults write com.apple.driver.AppleBluetoothMultitouch.trackpad TrackpadThreeFingerHorizSwipeGesture -bool false
+defaults write com.apple.driver.AppleBluetoothMultitouch.trackpad TrackpadThreeFingerTapGesture -bool false
+defaults write com.apple.driver.AppleBluetoothMultitouch.trackpad TrackpadThreeFingerVertSwipeGesture -bool false
+
+# Use scroll gesture with the Ctrl (^) modifier key to zoom
+defaults write com.apple.universalaccess closeViewScrollWheelToggle -bool true
+defaults write com.apple.universalaccess HIDScrollZoomModifierMask -int 262144
+
+# Turn off keyboard illumination when computer is not used for 10 seconds
+defaults write com.apple.BezelServices kDimTime -int 10
+
+# Don't create .DS_Store files on network drives
+defaults write com.apple.desktopservices DSDontWriteNetworkStores true
+
+# Enable fnKey
+defaults write -g com.apple.keyboard.fnState -boolean true
+
+# Change dock to only show active Apps
+defaults write com.apple.dock static-only -bool true
+
+# Set default browser
+open -a $HOME/Application/Google\ Chrome.app/Contents/MacOS/Google\ Chrome --make-default-browser 
+
+killList=( "SystemUIServer" "Dock" "Finder" "Google\ Chrome" )
+
+for app in "$(killList[@])"
+do
+  killall "$app" > /dev/null 2>&1
+done
---
+++ setup.sh
@@ -0,0 +1,31 @@
+function clone_dotfiles() {
+    git clone https://github.com/smilinmonki666/dotfiles.git $HOME/Documents/Projects/Dotfiles
+}
+
+function create_symlinks() {
+    docfiles="$HOME/Documents/Projects/personal/Dotfiles"
+
+    for file in $(find -L $docfiles -type f)
+    do
+    	ln -s $file $HOME/$(basename $file)
+    done
+
+    ln -s /Applications/Sublime\ Text\ 3.app/Contents/SharedSupport/bin/subl /usr/local/bin/subl
+}
+
+function replace_sublime_user_directory() {
+    sublimeDirectory="$HOME/Library/Application\ Support/Sublime\ Text\ 3/Packages/User"
+
+    echo "import urllib.request,os,hashlib; h = '7183a2d3e96f11eeadd761d777e62404' + 'e330c659d4bb41d3bdf022e94cab3cd0'; pf = 'Package Control.sublime-package'; ipp = sublime.installed_packages_path(); urllib.request.install_opener( urllib.request.build_opener( urllib.request.ProxyHandler()) ); by = urllib.request.urlopen( 'http://sublime.wbond.net/' + pf.replace(' ', '%20')).read(); dh = hashlib.sha256(by).hexdigest(); print('Error validating download (got %s instead of %s), please try manual install' % (dh, h)) if dh != h else open(os.path.join( ipp, pf), 'wb' ).write(by)" | pbcopy
+    echo "Submlime will open. Please press 'ctrl+`' to open the sublime console and paste the current clipboard & press enter to run. Then close sublime & return to this screen/window"
+    subl
+    echo "Press enter key to continue..."
+    read
+    rm -rf $sublimeDirectory
+    git clone http://github.com/smilinmonki666/sublime-user.git $sublimeDirectory
+    echo "Sublime Text will now open, leave it open as it will be installing all of the packages!"
+}
+
+clone_dotfiles \
+&& create_symlinks \
+&& replace_sublime_user_directory
