25c1cd37a01afbc2aafbe0899aca3dcc1e20d565
Nigel Greenway <nigel_greenway@icloud.com>
2014-08-10 20:48:08 +0100

    Remove old autocomplete dir symlink

--- setup.sh
+++ setup.sh
@@ -10,7 +10,6 @@ function create_symlinks() {
         ln -s $file $HOME/$(basename $file)
     done
 
-    ln -s $HOME/Documents/Projects/Dotfiles/Autocomplete $HOME/Autocomplete
     ln -s /opt/homebrew-cask/Caskroom/sublime-text3/Build\ 3059/Sublime\ Text.app/Contents/SharedSupport/bin/subl /usr/local/bin/subl
 }
 
