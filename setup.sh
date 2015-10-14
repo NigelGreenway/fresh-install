8c0ae50c438a8c6746dc483c67e49c6d2713f675
Nigel Greenway <nigel_greenway@me.com>
2015-10-14 20:00:05 +0100

    Update sublime 3 package manager install script

--- setup.sh
+++ setup.sh
@@ -16,7 +16,7 @@ function create_symlinks() {
 function replace_sublime_user_directory() {
     sublimeDirectory="$HOME/Library/Application\ Support/Sublime\ Text\ 3/Packages/User"
 
-    echo "import urllib.request,os,hashlib; h = '7183a2d3e96f11eeadd761d777e62404' + 'e330c659d4bb41d3bdf022e94cab3cd0'; pf = 'Package Control.sublime-package'; ipp = sublime.installed_packages_path(); urllib.request.install_opener( urllib.request.build_opener( urllib.request.ProxyHandler()) ); by = urllib.request.urlopen( 'http://sublime.wbond.net/' + pf.replace(' ', '%20')).read(); dh = hashlib.sha256(by).hexdigest(); print('Error validating download (got %s instead of %s), please try manual install' % (dh, h)) if dh != h else open(os.path.join( ipp, pf), 'wb' ).write(by)" | pbcopy
+    echo "import urllib.request,os,hashlib; h = '2915d1851351e5ee549c20394736b442' + '8bc59f460fa1548d1514676163dafc88'; pf = 'Package Control.sublime-package'; ipp = sublime.installed_packages_path(); urllib.request.install_opener( urllib.request.build_opener( urllib.request.ProxyHandler()) ); by = urllib.request.urlopen( 'http://packagecontrol.io/' + pf.replace(' ', '%20')).read(); dh = hashlib.sha256(by).hexdigest(); print('Error validating download (got %s instead of %s), please try manual install' % (dh, h)) if dh != h else open(os.path.join( ipp, pf), 'wb' ).write(by)" | pbcopy
     echo "Submlime will open. Please press 'ctrl+\`' to open the sublime console and paste the current clipboard & press enter to run. Then close sublime & return to this screen/window"
     subl
     echo "Press enter key to continue..."
